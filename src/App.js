d8ab75bb1dfaed8a4d47a53861cf55014bdef210
David Stinnett <davidstinnett@icloud.com>
2021-01-19 13:29:39 -0500

    fully functional, app pulls tours from list, you can delete tours and when no tours or left the page renders new info with a refresh button that brings back all tours from list

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
 	const [loading, setLoading] = useState(true);
 	const [tours, setTours] = useState([]);
+	const removeTour = (id) => {
+		const newTours = tours.filter((tour) => tour.id !== id);
+		setTours(newTours);
+	};
 
 	const fetchTours = async () => {
 		setLoading(true);
-		const response = await fetch(url);
-		const tours = await response.json();
-		console.log(tours);
+
+		try {
+			const response = await fetch(url);
+			const tours = await response.json();
+			setLoading(false);
+			setTours(tours);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
 	};
 	useEffect(() => {
 		fetchTours();
@@ -24,9 +35,22 @@ function App() {
 			</main>
 		);
 	}
+
+	if (tours.length === 0) {
+		return (
+			<main>
+				<div className="title">
+					<h2>No Tours Left</h2>
+					<button className="btn" onClick={() => fetchTours()}>
+						refresh
+					</button>
+				</div>
+			</main>
+		);
+	}
 	return (
 		<main>
-			<Tours />
+			<Tours tours={tours} removeTour={removeTour} />
 		</main>
 	);
 }
